c58c2f31af2e388967715ab2c940c5688edb41c8
RezaNaeemiRad <RezaNaeemiRad@gmail.com>
2021-01-14 18:14:08 +0330

    dynamic radblog article

--- blog/models.py
+++ blog/models.py
@@ -16,15 +16,20 @@ class UserProfile(models.Model):
     avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators=[validate_file_extension])
     description = models.CharField(max_length=512, null=False, blank=False)
 
-class Article(models.Model):
-    title = models.CharField(max_length=128, null=False, blank=False)
-    cover = models.FileField(upload_to='files/article_cover/', null=False, blank=False, validators=[validate_file_extension])
-    content = RichTextField()
-    created_at = models.DateTimeField(default=datetime.now, blank=False)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False, validators=[validate_file_extension])
 
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/article_cover/', null=False, blank=False, validators=[validate_file_extension])
+    content = RichTextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
